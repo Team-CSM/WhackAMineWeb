999359c1ee2c5359e5cfd83bbbbfb1056fb6bf2c
Craig Massie <2207400m@student.gla.ac.uk>
2018-03-16 17:32:06 +0000

    Updated download links. Skip button fixed.

--- TemplateData/js/whackamine.js
+++ TemplateData/js/whackamine.js
@@ -69,6 +69,6 @@ jumplink.addEventListener("click", function (event) {
   event.preventDefault();
   myvideo.play();
   myvideo.pause();
-  myvideo.currentTime = 62;
+  myvideo.currentTime = 70;
   myvideo.play();
 }, false);
